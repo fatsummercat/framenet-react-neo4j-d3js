25869ccbcc423c98416c1916dd2e9b22e1eec4c5
0xtanapat <tanapatdejchodanan@gmail.com>
2024-05-07 09:52:56 +0700

    chnage node size

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -92,7 +92,7 @@ const All = () => {
   const renderGraph = graph => {
     const width = 1500;
     const height = 600;
-    const nodeRadius = 25;
+    const nodeRadius = 16;
 
     const svg = d3
       .select(svgRef.current)
